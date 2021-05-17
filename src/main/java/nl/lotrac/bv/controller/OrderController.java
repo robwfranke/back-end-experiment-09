970a0b08da868d9eefcfd1f5cfe559053bbc961a
=Rob Franke <robwfranke@gmail.com>
2021-05-17 20:33:33 +0200

    orderController gefatsoeneerd

--- src/main/java/nl/lotrac/bv/controller/OrderController.java
+++ src/main/java/nl/lotrac/bv/controller/OrderController.java
@@ -25,80 +25,70 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-// CUSTOMER, COMPANY_USER, ADMIN
 
+//********************************************************************************
     @PostMapping(value = "/create")
     public ResponseEntity<Object> createNewOrder(@RequestBody Order order) {
-
         String newOrderName = orderService.createNewOrder(order);
-
         MessageFrontEnd message = new MessageFrontEnd("Order: " + newOrderName + "  created");
-
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{create}")
                 .buildAndExpand(newOrderName).toUri();
-
         return ResponseEntity.created(location).body(message);
     }
+//********************************************************************************
 
 
+//********************************************************************************
     @GetMapping(value = "")
     public ResponseEntity<Object> getAllOrders() {
-
         return ResponseEntity.ok().body(orderService.getAllOrders());
     }
+//********************************************************************************
 
-    //*************************************************************************************
+
+//********************************************************************************
     @GetMapping(value = "/ordersByCustomer/{username}")
     public ResponseEntity<Object> getAllordersByCustomer(@PathVariable("username") String username) {
-
-
         List<Order> orders = orderService.getAllOrdersByUser(username);
-
         return ResponseEntity.ok().body(orders);
     }
- //*************************************************************************************
+//********************************************************************************
 
 
-
-    //*************************************************************************************
+//********************************************************************************
     @GetMapping(value = "/inlog")
-
     public ResponseEntity<Object> getAllordersByInlogNameOnly() {
         String user = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
         List<Order> orders = orderService.getAllOrdersByUser(user);
-
         return ResponseEntity.ok().body(orders);
     }
-    //*************************************************************************************
+//********************************************************************************
 
 
-
-
-
-        @GetMapping(value = "/{id}")
-        public ResponseEntity<Order> getOneOrderByID (@PathVariable("id") Long id){
-            return new ResponseEntity<>(orderService.getOneOrderByID(id), HttpStatus.OK);
-        }
-
-
-//    in repository getOrderByOrdername
-
-        @GetMapping(value = "/name/{ordername}")
-        public ResponseEntity<Object> getOneOrderByName (@PathVariable("ordername") String ordername){
-            return ResponseEntity.ok().body(orderService.getOneOrderByName(ordername));
-        }
-
-
-        @PutMapping(value = "/update/{ordername}")
-
-        public ResponseEntity<Object> updateOrder (@PathVariable("ordername") String ordername, @RequestBody Order order)
-        {
-//public ResponseEntity<Object>updateOrder(@PathVariable("/update")String ordername, @RequestBody Order order){
-
-            orderService.updateOrder(ordername, order);
-            return ResponseEntity.noContent().build();
-
-        }
-
-
+//********************************************************************************
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> getOneOrderByID(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(orderService.getOneOrderByID(id), HttpStatus.OK);
     }
+//********************************************************************************
+
+
+//********************************************************************************
+    @GetMapping(value = "/name/{ordername}")
+    public ResponseEntity<Object> getOneOrderByName(@PathVariable("ordername") String ordername) {
+        return ResponseEntity.ok().body(orderService.getOneOrderByName(ordername));
+    }
+//********************************************************************************
+
+
+//********************************************************************************
+    @PutMapping(value = "/update/{ordername}")
+
+    public ResponseEntity<Object> updateOrder(@PathVariable("ordername") String ordername, @RequestBody Order order) {
+        orderService.updateOrder(ordername, order);
+        return ResponseEntity.noContent().build();
+    }
+//********************************************************************************
+
+
+}
